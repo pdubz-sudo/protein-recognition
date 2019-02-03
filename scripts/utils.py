075d4186c8ab993aeb2558f53f3ddace07204c3f
Philippe <philippewee@gmail.com>
2019-02-03 23:55:33 +0100

    na

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 
 def filter_example(b, r, g, y):
+    '''Displays filter images.'''
+
     fig, ax = plt.subplots(ncols=2, nrows=2, figsize=(12,12))
     fig.subplots_adjust(hspace=0.1)
 
@@ -11,10 +13,8 @@ def filter_example(b, r, g, y):
     ax[0,1].imshow(r)
     ax[0,1].set_title('red filter', fontsize=25)
 
-
     ax[1,0].imshow(g)
     ax[1,0].set_title('green filter', fontsize=25)
-
 
     ax[1,1].imshow(y)
     ax[1,1].set_title('yellow filter', fontsize=25)
